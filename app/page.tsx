56141cf0f159bbd04cd9f1e1662e69d2689cc800
EricFeng20001120 <fyx5620392@gmail.com>
2025-05-28 15:45:13 -0400

    implement companion list, cta

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,49 @@
-import { Button } from '@/components/ui/button'
+import CompanionCard from '@/components/companionCard'
+import CompanionsList from '@/components/companionsList'
+import Cta from '@/components/CTA'
+import { recentSessions } from '@/constants'
 import React from 'react'
 
 const Page = () => {
   return (
-    <div>
-      <h1 className='text-2xl underline'>Welcome to my SaaS App</h1>
-      <Button>
-        Lets get Started
-      </Button>
-    </div>
+    <main>
+      <h1 className='text-2xl underline'>Popular Companions</h1>
+      <section className='home-section'>
+        <CompanionCard
+          id = "1"
+          name = "a"
+          topic = "b"
+          subject = "c"
+          duration = {45}
+          color = "#ffda6e"
+        />
+        <CompanionCard
+          id = "1"
+          name = "a"
+          topic = "b"
+          subject = "c"
+          duration = {45}
+          color = "#ffda6e"
+        />
+        <CompanionCard
+          id = "3"
+          name = "a"
+          topic = "b"
+          subject = "c"
+          duration = {45}
+          color = "#ffda6e"
+        />
+      </section>
+
+      <section className='home-section'>
+        <CompanionsList
+          title = "Recently completed sessions"
+          companions = {recentSessions}
+          classNames = "w-2/3 max-lg:w-full"
+        />
+        <Cta/>
+      </section>
+    </main>
   )
 }
 
